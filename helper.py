5c78ffb06cbf67146f09f761c42a96af9e72049f
LeanderLXZ <466532329@qq.com>
2017-06-06 17:27:41 +0800

    Modify helper.py and delete matplotlib to fit linux.

--- helper.py
+++ helper.py
@@ -1,6 +1,6 @@
 import pickle
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelBinarizer
 
 
@@ -24,37 +24,37 @@ def load_cfar10_batch(cifar10_dataset_folder_path, batch_id):
     return features, labels
 
 
-def display_stats(cifar10_dataset_folder_path, batch_id, sample_id):
-    """
-    Display Stats of the the dataset
-    """
-    batch_ids = list(range(1, 6))
+#def display_stats(cifar10_dataset_folder_path, batch_id, sample_id):
+#    """
+#    Display Stats of the the dataset
+#    """
+#    batch_ids = list(range(1, 6))
 
-    if batch_id not in batch_ids:
-        print('Batch Id out of Range. Possible Batch Ids: {}'.format(batch_ids))
-        return None
+#    if batch_id not in batch_ids:
+#        print('Batch Id out of Range. Possible Batch Ids: {}'.format(batch_ids))
+#        return None
 
-    features, labels = load_cfar10_batch(cifar10_dataset_folder_path, batch_id)
+#    features, labels = load_cfar10_batch(cifar10_dataset_folder_path, batch_id)
 
-    if not (0 <= sample_id < len(features)):
-        print('{} samples in batch {}.  {} is out of range.'.format(len(features), batch_id, sample_id))
-        return None
+#    if not (0 <= sample_id < len(features)):
+#        print('{} samples in batch {}.  {} is out of range.'.format(len(features), batch_id, sample_id))
+#        return None
 
-    print('\nStats of batch {}:'.format(batch_id))
-    print('Samples: {}'.format(len(features)))
-    print('Label Counts: {}'.format(dict(zip(*np.unique(labels, return_counts=True)))))
-    print('First 20 Labels: {}'.format(labels[:20]))
+#    print('\nStats of batch {}:'.format(batch_id))
+#    print('Samples: {}'.format(len(features)))
+#    print('Label Counts: {}'.format(dict(zip(*np.unique(labels, return_counts=True)))))
+#    print('First 20 Labels: {}'.format(labels[:20]))
 
-    sample_image = features[sample_id]
-    sample_label = labels[sample_id]
-    label_names = _load_label_names()
+#    sample_image = features[sample_id]
+#    sample_label = labels[sample_id]
+#    label_names = _load_label_names()
 
-    print('\nExample of Image {}:'.format(sample_id))
-    print('Image - Min Value: {} Max Value: {}'.format(sample_image.min(), sample_image.max()))
-    print('Image - Shape: {}'.format(sample_image.shape))
-    print('Label - Label Id: {} Name: {}'.format(sample_label, label_names[sample_label]))
-    plt.axis('off')
-    plt.imshow(sample_image)
+#    print('\nExample of Image {}:'.format(sample_id))
+#    print('Image - Min Value: {} Max Value: {}'.format(sample_image.min(), sample_image.max()))
+#    print('Image - Shape: {}'.format(sample_image.shape))
+#    print('Label - Label Id: {} Name: {}'.format(sample_label, label_names[sample_label]))
+#    plt.axis('off')
+#    plt.imshow(sample_image)
 
 
 def _preprocess_and_save(normalize, one_hot_encode, features, labels, filename):
@@ -135,33 +135,33 @@ def load_preprocess_training_batch(batch_id, batch_size):
     return batch_features_labels(features, labels, batch_size)
 
 
-def display_image_predictions(features, labels, predictions):
-    n_classes = 10
-    label_names = _load_label_names()
-    label_binarizer = LabelBinarizer()
-    label_binarizer.fit(range(n_classes))
-    label_ids = label_binarizer.inverse_transform(np.array(labels))
+#def display_image_predictions(features, labels, predictions):
+#    n_classes = 10
+#    label_names = _load_label_names()
+#    label_binarizer = LabelBinarizer()
+#    label_binarizer.fit(range(n_classes))
+#    label_ids = label_binarizer.inverse_transform(np.array(labels))
 
-    fig, axies = plt.subplots(nrows=4, ncols=2)
-    fig.tight_layout()
-    fig.suptitle('Softmax Predictions', fontsize=20, y=1.1)
+#    fig, axies = plt.subplots(nrows=4, ncols=2)
+#    fig.tight_layout()
+#    fig.suptitle('Softmax Predictions', fontsize=20, y=1.1)
 
-    n_predictions = 3
-    margin = 0.05
-    ind = np.arange(n_predictions)
-    width = (1. - 2. * margin) / n_predictions
+#    n_predictions = 3
+#    margin = 0.05
+#    ind = np.arange(n_predictions)
+#    width = (1. - 2. * margin) / n_predictions
 
-    for image_i, (feature, label_id, pred_indicies, pred_values) in enumerate(zip(features, label_ids, predictions.indices, predictions.values)):
-        pred_names = [label_names[pred_i] for pred_i in pred_indicies]
-        correct_name = label_names[label_id]
+#    for image_i, (feature, label_id, pred_indicies, pred_values) in enumerate(zip(features, label_ids, predictions.indices, predictions.values)):
+#        pred_names = [label_names[pred_i] for pred_i in pred_indicies]
+#        correct_name = label_names[label_id]
 
-        axies[image_i][0].imshow(feature)
-        axies[image_i][0].set_title(correct_name)
-        axies[image_i][0].set_axis_off()
+#        axies[image_i][0].imshow(feature)
+#        axies[image_i][0].set_title(correct_name)
+#        axies[image_i][0].set_axis_off()
 
-        axies[image_i][1].barh(ind + margin, pred_values[::-1], width)
-        axies[image_i][1].set_yticks(ind + margin)
-        axies[image_i][1].set_yticklabels(pred_names[::-1])
-        axies[image_i][1].set_xticks([0, 0.5, 1.0])
+#        axies[image_i][1].barh(ind + margin, pred_values[::-1], width)
+#        axies[image_i][1].set_yticks(ind + margin)
+#        axies[image_i][1].set_yticklabels(pred_names[::-1])
+#        axies[image_i][1].set_xticks([0, 0.5, 1.0])
 
-    plt.show()
+#    plt.show()
